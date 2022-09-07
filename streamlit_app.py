bc49a64b08aa232aadad5e758bb6bcdc8391b11f
lian.jiang <lian.jiang@opendoor.com>
2022-09-07 09:56:34 -0700

    remove phone number

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,4 @@ rows = run_query("SELECT CUSTOMER_UUID from DWH_DEV.LIAN_TEST.PHONE_NUMBERCUSTOM
 
 # Print results.
 for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+    st.write(f"got {row[0]}")
